5028628346c25ea8a31cda0cb23fb918cfafc699
Vladimir Shiryaev <109172834+wowqasql@users.noreply.github.com>
2023-09-24 20:07:15 +0300

    update_v2
    update_v2

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 // 3D Scroll
 
 let zSpacing = -900,
-		lastPos = zSpacing / 5,
-		$frames = document.getElementsByClassName('frame'),
-		frames = Array.from($frames),
-		zVals = []
+	lastPos = zSpacing / 5,
+	$frames = document.getElementsByClassName('frame'),
+	frames = Array.from($frames),
+	zVals = []
 
-window.onscroll = function() {
+window.onscroll = function () {
 
 	let top = document.documentElement.scrollTop,
-			delta = lastPos - top
+		delta = lastPos - top
 
 	lastPos = top
 
-	frames.forEach(function(n, i) {
+	frames.forEach(function (n, i) {
 		zVals.push((i * zSpacing) + zSpacing)
 		zVals[i] += delta * -5.5
 		let frame = frames[i],
-				transform = `translateZ(${zVals[i]}px)`,
-				opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
+			transform = `translateZ(${zVals[i]}px)`,
+			opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
 		frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
 	})
 
@@ -29,17 +29,83 @@ window.scrollTo(0, 1)
 // Audio
 
 let soundButton = document.querySelector('.soundbutton'),
-		audio = document.querySelector('.audio')
+	audio = document.querySelector('.audio')
 
 soundButton.addEventListener('click', e => {
 	soundButton.classList.toggle('paused')
 	audio.paused ? audio.play() : audio.pause()
 })
 
-window.onfocus = function() {
+window.onfocus = function () {
 	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
 }
 
-window.onblur = function() {
+window.onblur = function () {
 	audio.pause()
 }
+
+// Animations
+const fading5 = document.querySelector('.fading-5')
+
+
+setInterval(() => {
+	fading5.style.backgroundPosition = `bottom left`
+	fading5.style.backgroundSize = `0 0`
+
+}, 1000)
+
+window.addEventListener("scroll", () => {
+
+	if (window.scrollY >= 100) {
+		const thick3 = document.querySelector('.thick-3')
+		thick3.style.background = `linear-gradient(#E3C53A 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
+		thick3.style.transition = `1s, background-position 1s 1s, color 1s 1s`
+		thick3.style.backgroundPosition = `50% 0%`
+		thick3.style.color = `#000`
+	}
+
+	if (window.scrollY >= 600) {
+		const hardSkills = document.querySelector('.thick-3-hrd_skills')
+		hardSkills.style.background = `linear-gradient(#1841F4 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
+		hardSkills.style.transition = `1s, background-position 1s 1s, color 1s 1s`
+		hardSkills.style.backgroundPosition = `50% 0%`
+		hardSkills.style.color = `#000`
+	}
+
+	if (window.scrollY >= 1200) {
+		const hardSkills = document.querySelector('.thick-3-soft_skills')
+		hardSkills.style.background = `linear-gradient(#804EF6 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
+		hardSkills.style.transition = `1s, background-position 1s 1s, color 1s 1s`
+		hardSkills.style.backgroundPosition = `50% 0%`
+		hardSkills.style.color = `#000`
+	}
+	if (window.scrollY >= 1600) {
+		const hardSkills = document.querySelector('.thick-3-work_exp')
+		hardSkills.style.background = `linear-gradient(#ED5A05 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
+		hardSkills.style.transition = `1s, background-position 1s 1s, color 1s 1s`
+		hardSkills.style.backgroundPosition = `50% 0%`
+		hardSkills.style.color = `#000`
+	}
+	if (window.scrollY >= 2200) {
+		const hardSkills = document.querySelector('.thick-3-freelance')
+		hardSkills.style.background = `linear-gradient(#F5707D 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
+		hardSkills.style.transition = `1s, background-position 1s 1s, color 1s 1s`
+		hardSkills.style.backgroundPosition = `50% 0%`
+		hardSkills.style.color = `#000`
+	}
+
+	if (window.scrollY >= 2650) {
+		const hardSkills = document.querySelector('.thick-3-about')
+		hardSkills.style.background = `linear-gradient(#00B2DF 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
+		hardSkills.style.transition = `1s, background-position 1s 1s, color 1s 1s`
+		hardSkills.style.backgroundPosition = `50% 0%`
+		hardSkills.style.color = `#000`
+	}
+
+	if (window.scrollY >= 3050) {
+		const unexpected4 = document.querySelector('.unexpected-4')
+		unexpected4.style.background = `linear-gradient(#E3C53A 0 0) 0% 100% /100% 2px no-repeat`
+		unexpected4.style.transition = `2s, background-size 2s 1.5s`
+
+	}
+})
