6bca0003f4fbc289471db23388437d2316b14735
Vladimir Shiryaev <109172834+wowqasql@users.noreply.github.com>
2023-10-01 17:54:46 +0300

    up_animations

--- js/app.js
+++ js/app.js
@@ -43,40 +43,87 @@ window.onfocus = function () {
 window.onblur = function () {
 	audio.pause()
 }
+const root = document.querySelector(':root')
 
 // Animations
+const followButton = document.querySelector('.fab-action-button')
+
+const animationRight = document.querySelector('.text-right')
+const mediaLeft = document.querySelector('.frame-media_left')
+
+const hardSkillsLeft = document.querySelector('.hard_skills__left')
+const hardSkillsRight = document.querySelector('.hard_skills__right')
+
+const softSkillsLeft = document.querySelector('.soft-skills__left')
+const softSkillsRight = document.querySelector('.soft-skills__right')
+
 
 
 window.addEventListener("scroll", () => {
 	if (window.scrollY >= 0) {
+
 		const line = document.querySelector('.line')
 		line.style.background = `linear-gradient(blueviolet 0 0) 0% 100% /100% 3px no-repeat`
 		line.style.transition = `2s, background-size 2s 1.5s`
+		setTimeout(() => {
+			line.style.opacity = 0;
+		}, 2000)
 
 	}
 
 	if (window.scrollY >= 100) {
-		const thick3 = document.querySelector('.thick-3')
-		thick3.style.background = `linear-gradient(#E3C53A 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
-		thick3.style.transition = `1s, background-position 1s 1s, color 1s 1s`
-		thick3.style.backgroundPosition = `0% 0%`
-		thick3.style.color = `#000`
+
+		animationRight.classList.add('animation_right')
+		mediaLeft.classList.add('animation_left')
+
+		setTimeout(() => {
+			const thick3 = document.querySelector('.thick-3')
+			thick3.style.background = `linear-gradient(#E3C53A 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
+			thick3.style.transition = `1s, background-position 1s 1s, color 1s 1s`
+			thick3.style.backgroundPosition = `0% 0%`
+			thick3.style.color = `#000`
+
+			// root.style.setProperty('--color-follow__button', '#E3C53A');
+
+
+		}, 1600)
+
+
+
 	}
+
 
 	if (window.scrollY >= 600) {
-		const hardSkills = document.querySelector('.thick-3-hrd_skills')
-		hardSkills.style.background = `linear-gradient(#1841F4 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
-		hardSkills.style.transition = `1s, background-position 1s 1s, color 1s 1s`
-		hardSkills.style.backgroundPosition = `50% 0%`
-		hardSkills.style.color = `#000`
+
+		hardSkillsLeft.classList.add('animation_left__hard-skills__left')
+		hardSkillsRight.classList.add('animation_right__hard-skills__right')
+
+		setTimeout(() => {
+			const hardSkills = document.querySelector('.thick-3-hrd_skills')
+			hardSkills.style.background = `linear-gradient(#1841F4 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
+			hardSkills.style.transition = `1s, background-position 1s 1s, color 1s 1s`
+			hardSkills.style.backgroundPosition = `50% 0%`
+			hardSkills.style.color = `#000`
+
+			// root.style.setProperty('--color-follow__button', '#1841F4');
+
+		}, 400)
+
 	}
 
-	if (window.scrollY >= 1200) {
-		const hardSkills = document.querySelector('.thick-3-soft_skills')
-		hardSkills.style.background = `linear-gradient(#804EF6 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
-		hardSkills.style.transition = `1s, background-position 1s 1s, color 1s 1s`
-		hardSkills.style.backgroundPosition = `50% 0%`
-		hardSkills.style.color = `#000`
+	if (window.scrollY >= 1100) {
+
+		softSkillsLeft.classList.add('animation_left__soft-skills__left')
+		softSkillsRight.classList.add('animation_right__soft-skills__right')
+
+		setTimeout(() => {
+
+			const hardSkills = document.querySelector('.thick-3-soft_skills')
+			hardSkills.style.background = `linear-gradient(#804EF6 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
+			hardSkills.style.transition = `1s, background-position 1s 1s, color 1s 1s`
+			hardSkills.style.backgroundPosition = `50% 0%`
+			hardSkills.style.color = `#000`
+		}, 1500)
 	}
 	if (window.scrollY >= 1600) {
 		const hardSkills = document.querySelector('.thick-3-work_exp')
@@ -85,7 +132,14 @@ window.addEventListener("scroll", () => {
 		hardSkills.style.backgroundPosition = `50% 0%`
 		hardSkills.style.color = `#000`
 	}
-	if (window.scrollY >= 2200) {
+	if (window.scrollY >= 2100) {
+		const workExp = document.querySelector('.thick-3-work_exp-2')
+		workExp.style.background = `linear-gradient(#FFC3A8 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
+		workExp.style.transition = `1s, background-position 1s 1s, color 1s 1s`
+		workExp.style.backgroundPosition = `50% 0%`
+		workExp.style.color = `#000`
+
+
 		const hardSkills = document.querySelector('.thick-3-freelance')
 		hardSkills.style.background = `linear-gradient(#F5707D 0 0) 0% calc(-3px - 100%) /100% 200% no-repeat`
 		hardSkills.style.transition = `1s, background-position 1s 1s, color 1s 1s`
@@ -163,4 +217,39 @@ stars3.style.boxShadow = multipleBoxShadow(200);
 // starsAfter3.style.boxShadow = multipleBoxShadow(2000);
 
 
+const scrollTop = (top, timer) => {
+	setTimeout(() => {
+		window.scrollBy({
+			top,
+			behavior: 'smooth',
+			right: 200
 
+		});
+	}, timer)
+}
+
+// поменять на конкретные пиксели страницы
+scrollTop(250, 22000)
+scrollTop(400, 40000)
+scrollTop(500, 51000)
+scrollTop(500, 65000)
+scrollTop(500, 70000)
+scrollTop(500, 80000)
+scrollTop(500, 90000)
+
+
+
+const start = document.querySelector('.start')
+
+
+start.addEventListener('click', () => {
+
+	setTimeout(() => {
+		window.scrollBy({
+			top: 200,
+			behavior: 'smooth'
+
+		}, 10000);
+	})
+
+})
